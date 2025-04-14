1ea5f4b01132a7793f5ece3626cd8e23794d149a
Your Name <your@email.com>
2025-04-14 13:03:16 -0400

    Update from Python script

--- updater.py
+++ updater.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python3
-
 import subprocess
-import os
+
 
 def update_github_repo():
     """
     This script will:
     1. Initialize (or re-initialize) the current directory as a Git repository.
-    2. Add the GitHub remote 'origin' pointing to https://github.com/tomat0ketchum/SSP.git
+    2. Add the GitHub remote 'origin' pointing to
+    https://github.com/tomat0ketchum/SSP.git
     3. Stage all files (git add .)
     4. Commit with a default message.
     5. Push to the 'main' branch on GitHub.
@@ -19,12 +18,14 @@ def update_github_repo():
     # 2. Set up remote origin (overwrites if it already exists)
     subprocess.run(["git", "remote", "remove", "origin"], check=False)
     subprocess.run(
-        ["git", "remote", "add", "origin", "https://github.com/tomat0ketchum/SSP.git"],
+        ["git", "remote", "add", "origin",
+            "https://github.com/tomat0ketchum/SSP.git"],
         check=True
     )
 
     # Optionally pull from the remote to merge if there's existing work:
-    # subprocess.run(["git", "pull", "origin", "main", "--allow-unrelated-histories"], check=False)
+    # subprocess.run(["git", "pull", "origin", "main",
+    # "--allow-unrelated-histories"], check=False)
 
     # 3. Stage all files
     subprocess.run(["git", "add", "."], check=True)
@@ -34,10 +35,12 @@ def update_github_repo():
     subprocess.run(["git", "commit", "-m", commit_message], check=True)
 
     # 5. Push to the 'main' branch on GitHub
-    # (If your local branch is 'master', change 'main' to 'master' or whichever branch you use)
+    # (If your local branch is 'master', change 'main' to 'master'
+    # or whichever branch you use)
     subprocess.run(["git", "push", "-u", "origin", "main"], check=True)
 
 
 if __name__ == "__main__":
-    # Make sure you run this script from the directory containing the files you want to push
+    # Make sure you run this script from the directory
+    # containing the files you want to push
     update_github_repo()
